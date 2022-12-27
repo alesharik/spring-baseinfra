98f1d7d2f733fff5cd78c91bb4ceca7d509d1ba1
Aleksei Arsenev <alesharik4@gmail.com>
2022-12-27 18:50:18 +0300

    chore(file-storage/minio): update to Spring Boot 3

--- libs/file-storage/minio/build.gradle.kts
+++ libs/file-storage/minio/build.gradle.kts
@@ -5,8 +5,9 @@ dependencies {
     api(project(":common-minio"))
     implementation(libs.spring.starter.sentry)
     implementation(libs.spring.boot)
+    implementation(libs.spring.web)
     implementation(libs.spring.boot.autoconfigure)
+    implementation(libs.slf4j)
     implementation(libs.bundles.springdoc)
-    implementation(libs.problems.web)
     testImplementation(libs.spring.starter.test)
 }
