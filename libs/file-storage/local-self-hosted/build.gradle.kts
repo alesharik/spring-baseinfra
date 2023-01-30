41f5e144503393e4b860e1598897a1098ed3d221
Aleksei Arsenev <alesharik4@gmail.com>
2023-01-30 15:59:24 +0300

    fix(file-storage): fix buildfiles

--- libs/file-storage/local-self-hosted/build.gradle.kts
+++ libs/file-storage/local-self-hosted/build.gradle.kts
@@ -1,11 +1,10 @@
-version = "1.0.1"
+version = "1.0.2"
 
 dependencies {
     api(project(":file-storage-api"))
     implementation(libs.spring.web)
-    implementation(libs.problems.web)
     implementation(libs.spring.boot)
     implementation(libs.spring.boot.autoconfigure)
-    implementation(libs.bundles.springdoc)
+    implementation(libs.swagger.annotations)
     testImplementation(libs.spring.starter.test)
 }
--- libs/file-storage/minio/build.gradle.kts
+++ libs/file-storage/minio/build.gradle.kts
@@ -1,12 +1,12 @@
-version = "1.0.1"
+version = "1.0.2"
 
 dependencies {
     api(project(":file-storage-api"))
     api(project(":common-minio"))
     implementation(libs.spring.starter.sentry)
     implementation(libs.spring.boot)
+    implementation(libs.spring.web)
     implementation(libs.spring.boot.autoconfigure)
-    implementation(libs.bundles.springdoc)
-    implementation(libs.problems.web)
+    implementation(libs.slf4j)
     testImplementation(libs.spring.starter.test)
 }
